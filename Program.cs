5d28db648522734db73047dec4c2cba6e1f0f4ae
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-10-23 14:54:55 -0300

    remove usings no program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 using Dominando_Injecao_Depencia.Configuration;
 using Dominando_Injecao_Depencia.Extensions;
-using Microsoft.Data.SqlClient;
-using Repositories;
-using Repositories.Interfaces;
-using Services;
-using Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<Configuration>();
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddRepositories();
 builder.Services.AddServices();
-builder.Services.AddSqlConnection("CONN_STRING");
+builder.Services.AddSqlConnection(connStr);
 builder.Services.AddControllers();
 var app = builder.Build();
 
