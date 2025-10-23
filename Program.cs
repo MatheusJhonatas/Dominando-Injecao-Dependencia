2028cc00d19bc800794b1e1d269d1f45527d93de
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-10-22 21:31:53 -0300

    new id

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using Repositories;
 using Repositories.Interfaces;
+using Services;
+using Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
 
 builder.Services.AddControllers();
 var app = builder.Build();
