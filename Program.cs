44f6ed9657e8395198dc6c55b95bc33ef7543ecc
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-10-22 21:30:50 -0300

    add injeção de dependencia

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Repositories;
+using Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+
 builder.Services.AddControllers();
 var app = builder.Build();
 
