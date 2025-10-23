e71757f82b237e99ce75a3f171f35a66c2bbacaf
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-10-22 21:48:48 -0300

    add class configuration

---
+++ Configuration.cs
@@ -0,0 +1,6 @@
+namespace Dominando_Injecao_Depencia.Configuration;
+
+public class Configuration
+{
+    public string DeliveryFeeServiceUrl { get; set; } = "";
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
+using Microsoft.Data.SqlClient;
 using Repositories;
 using Repositories.Interfaces;
 using Services;
 using Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+#region AddScoped
+//Add scoped sempre que for durante o ciclo de vida da requisicao a mesma instancia do objeto
+builder.Services.AddScoped(c => new SqlConnection("CONN_STRING"));
+#endregion
+#region AddSingleton
+//Add singleton sempre que for durante toda a aplicacao a mesma instancia do objeto
 
+#endregion
+#region AddTransient
+//Add transient sempre que for cada requisicao uma nova instancia do objeto
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
-
+#endregion
 builder.Services.AddControllers();
 var app = builder.Build();
 
