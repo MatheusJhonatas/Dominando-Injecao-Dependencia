81506d99d4feaecffa551c531781988bb54a9371
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-10-23 14:55:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 using Dominando_Injecao_Depencia.Configuration;
 using Dominando_Injecao_Depencia.Extensions;
 
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<Configuration>();
-var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddRepositories();
 builder.Services.AddServices();
-builder.Services.AddSqlConnection(connStr);
+builder.Services.AddSqlConnection("CONN_STRING");
 builder.Services.AddControllers();
 var app = builder.Build();
 
